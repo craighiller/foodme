7355c340ace2652893a7294bfda1fc2452b85eee
Craig Hiller <chiller@berkeley.edu>
2014-03-01 20:10:24 -0800

    working texts

--- texter.py
+++ texter.py
@@ -1,15 +1,4 @@
-import smtplib
+from google.appengine.api import mail
 
 def text(number, message):
-    fromaddr = 'foodme14@gmail.com'
-    toaddrs  = str(number)+'@txt.att.net'
-    msg = message
-
-    username = 'foodme14@gmail.com'
-    password = 'foodme14password'
-
-    server = smtplib.SMTP('smtp.gmail.com:587')
-    server.starttls()
-    server.login(username,password)
-    server.sendmail(fromaddr, toaddrs, msg)
-    server.quit()
+    mail.send_mail(sender="FoodMe <foodme14@gmail.com>", to=str(number)+'@txt.att.net', body=message, subject="Food Plz")
